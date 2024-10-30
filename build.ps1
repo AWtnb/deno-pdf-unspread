3fa26f666926489b9b8a9d100706720eb0bcfd30
awtnb <arikaebanataw@gmail.com>
2024-10-30 13:36:57 +0900

    update builder

--- build.ps1
+++ build.ps1
@@ -1,13 +1,13 @@
-$EXENAME = $args[0]
-if ($EXENAME.length -lt 1) {
-    $EXENAME = (($pwd).Path | Split-Path -Leaf) -replace "^deno-"
-}
+$EXENAME = ($pwd).Path | Split-Path -Leaf
 
-deno compile --allow-import --allow-read --allow-write --output $EXENAME .\main.ts
+deno compile --allow-import --allow-read --allow-write .\main.ts
 
 if ($LASTEXITCODE -eq 0) {
     if (Test-Path .\.env) {
         $d = (Get-Content .\.env -Raw).Trim()
+        if ($d.Length -lt 1) {
+            $d = $env:USERPROFILE | Join-Path -ChildPath "tools\bin"
+        }
         $d = [System.Environment]::ExpandEnvironmentVariables($d)
         if (-not (Test-Path $d -PathType Container)) {
             New-Item -Path $d -ItemType Directory
